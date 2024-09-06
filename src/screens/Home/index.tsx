2c1ff12a3c7155ac8075766712a3124bdd14d7d8
Lucas Scotti <lucas_scotti.13@hotmail.com>
2024-09-06 20:53:39 -0300

    feat: Criação do Input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export function Home() {
   return (
@@ -8,7 +9,17 @@ export function Home() {
         <Text style={styles.title}>Lista de Compras</Text>
       </View>
       <View style={styles.mainContainer}>
-        
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione um novo produto"
+            placeholderTextColor={"#808080"}
+          ></TextInput>
+          <TouchableOpacity style={styles.button}>
+            <AntDesign name="pluscircleo" size={18} color="#fff" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.infoContainer}></View>
       </View>
     </View>
   );
